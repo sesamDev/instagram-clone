fb823709df1fca159109628ed90e1adae7013944
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-28 20:38:23 +0100

    Started adding main flow card

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,10 +1,12 @@
 import Card from "../card";
 import React from "react";
+import { signOutUser } from "../../App";
 
 const Home = () => {
   return (
     <div>
       <Card />
+      <button onClick={signOutUser}>Sign out</button>
     </div>
   );
 };
