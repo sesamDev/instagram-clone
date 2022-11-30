1cd939e4125d5d201f32d6ce3cc76486b93e9554
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-11-30 19:05:09 +0100

    removed ph img

--- src/components/card.js
+++ src/components/card.js
@@ -56,7 +56,7 @@ const savePostButton = (
   </svg>
 );
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       <div className="author">
@@ -65,7 +65,7 @@ const Card = () => {
       </div>
 
       <div className="cardContent">
-        <img src={imagePH} alt="content" />
+        <img src={props.img} alt="content" />
       </div>
       <div className="actionButtons">
         <span>{likeButton}</span>
